5e7adc02c5b441c20818d9671d5deb2b6ab7f136
Adrian Grygutis <gryguc7@gmail.com>
2022-11-27 08:54:29 +0100

    book list scrollbar and searching

---
+++ src/jvmMain/kotlin/pl/feureth/view/desktop/components/ScrollBar.kt
@@ -0,0 +1,76 @@
+package pl.feureth.view.desktop.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.coerceIn
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ScrollBar(scrollState: LazyListState, modifier: Modifier = Modifier) {
+
+    val density = LocalDensity.current
+    val firstVisibleItem = scrollState.firstVisibleItemIndex
+    val visibleItems = scrollState.layoutInfo.visibleItemsInfo.size
+    val itemsCount = scrollState.layoutInfo.totalItemsCount
+    var viewHeightPx by remember { mutableStateOf(0) }
+
+    val viewHeight by remember(viewHeightPx) {
+        derivedStateOf {
+            with(density) { viewHeightPx.toDp() }
+        }
+    }
+//    val barHeight by remember(viewHeight, visibleItems, itemsCount) {
+//        derivedStateOf {
+//            viewHeight * (visibleItems / itemsCount.toFloat())
+//        }
+//    }
+    val barHeight = 200.dp
+
+    var isDragging by remember { mutableStateOf(false) }
+    var manualBarPosition by remember { mutableStateOf(0.dp) }
+    val barPosition by derivedStateOf {
+        if (isDragging) {
+            manualBarPosition
+        } else {
+            viewHeight * (firstVisibleItem / itemsCount.toFloat())
+        }
+    }
+
+    Box(
+        modifier = Modifier.width(8.dp).fillMaxHeight().background(Color.LightGray)
+            .then(modifier)
+            .onSizeChanged {
+                viewHeightPx = it.height
+            }
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth().height(barHeight).offset(y = barPosition)
+                .background(MaterialTheme.colors.primary, RoundedCornerShape(.5f))
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDragStart = {
+                            manualBarPosition = barPosition
+                            isDragging = true
+                        },
+                        onDrag = { change, dragAmount ->
+                            manualBarPosition =
+                                (manualBarPosition + dragAmount.y.toDp()).coerceIn(0.dp, viewHeight - barHeight)
+                        },
+                        onDragEnd = {
+                            isDragging = false
+                        }
+                    )
+                }
+        )
+    }
+}
--- src/jvmMain/kotlin/pl/feureth/view/desktop/screen/book/BookListScreen.kt
+++ src/jvmMain/kotlin/pl/feureth/view/desktop/screen/book/BookListScreen.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import pl.feureth.model.Book
 import pl.feureth.presenter.book.BookRepository
+import pl.feureth.view.desktop.components.ScrollBar
 
 @Composable
 fun BookListScreen(
@@ -22,27 +26,42 @@ fun BookListScreen(
     modifier: Modifier = Modifier
 ) {
 
-    var books by remember { mutableStateOf(bookRepository.getBooks()) }
+    val books by remember { mutableStateOf(bookRepository.getBooks()) }
+    val listScrollState = rememberLazyListState()
+    var strSearchQuery by remember { mutableStateOf("") }
 
-    LazyColumn(modifier = modifier) {
-        item {
-            Text("Książki", style = MaterialTheme.typography.h6)
-        }
-        item {
-            Button(
-                onClick = {
-                    bookRepository.insert(Book(id = 0L, title = "T3st", author = "Test"))
-                    books = bookRepository.getBooks()
-                }
+    Column(modifier = modifier) {
+        Text("Książki", style = MaterialTheme.typography.h3)
+        TextField(
+            value = strSearchQuery,
+            onValueChange = { strSearchQuery = it },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text("Wyszukaj...") }
+        )
+        Spacer(
+            modifier = Modifier.fillMaxWidth().height(1.dp).padding(end = 16.dp).background(Color.Gray)
+                .padding(vertical = 8.dp)
+        )
+
+        Row(modifier = Modifier.weight(1f).fillMaxWidth()) {
+            LazyColumn(
+                modifier = Modifier.weight(1f).fillMaxHeight(),
+                contentPadding = PaddingValues(vertical = 8.dp),
+                state = listScrollState
             ) {
-                Text("Dodaj testowo")
+                items(books.filter {
+                    it.title.contains(strSearchQuery, ignoreCase = true) ||
+                            it.author.contains(strSearchQuery, ignoreCase = true)
+                }) {
+                    BookItem(it)
+                }
             }
-        }
-        item {
-            Spacer(modifier = Modifier.fillMaxWidth().height(1.dp).background(Color.Gray).padding(vertical = 8.dp))
-        }
-        items(books) {
-            BookItem(it)
+            if (listScrollState.layoutInfo.totalItemsCount > 0) {
+                ScrollBar(
+                    scrollState = listScrollState,
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
         }
     }
 }
@@ -51,12 +70,14 @@ fun BookListScreen(
 fun BookItem(book: Book) {
     Row(
         modifier = Modifier.fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .padding(vertical = 4.dp)
+            .padding(end = 16.dp)
             .shadow(4.dp, shape = RoundedCornerShape(8.dp))
             .background(Color.White, RoundedCornerShape(8.dp))
-            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(modifier = Modifier.weight(1f)) {
+        Column(modifier = Modifier.weight(1f).fillMaxHeight()) {
             Text(book.title, color = Color.Black, style = MaterialTheme.typography.body1)
             Text(book.author, color = Color.Gray, style = MaterialTheme.typography.body2)
         }
